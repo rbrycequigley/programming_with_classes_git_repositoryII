b9dcf1723f5f46e90c3ca611d6fca2722f31b65e
bryce <qui21023@byui.edu>
2026-01-13 16:09:15 -0700

    Compeleted assignment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,19 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Prep 01
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
+
     }
+        
+
 }
